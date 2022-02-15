0d4bd6627509365dbcfd4061a621b4f5889f1991
Twedmark <axel.twedmark@hotmail.com>
2022-02-15 17:32:26 +0100

    Update ticket.js
    så ser barcoden lite annurlunda ut på varje ticket

--- frontend/ticket.js
+++ frontend/ticket.js
@@ -4,6 +4,7 @@ const dateElem = document.getElementById('date');
 const timeFromElem = document.getElementById('timeFrom');
 const timeToElem = document.getElementById('timeTo');
 const idElem = document.getElementById('ticketId');
+const barcodeElem = document.getElementById('barcode');
 
 
 
@@ -17,7 +18,7 @@ async function getTicketDetails() {
   timeFromElem.innerHTML = data.timeStart;
   timeToElem.innerHTML = data.timeStop;
   idElem.innerHTML = "Biljetnummer: " + data.id;
-
+  barcodeElem.innerHTML = data.id;
 }
 
 getTicketDetails();
